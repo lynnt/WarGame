6cecb59a9a8e4c610bdcd9be0b5b336eeb3cf526
Lynn Tran <lynn.tran18@gmail.com>
2014-08-26 19:20:26 -0400

    Added the card

--- wargame.cc
+++ wargame.cc
@@ -10,21 +10,92 @@
 
 using namespace std;
 
+/*
+ * The deck will start from 0 to 51 where the suits are clubs, diamonds, hearts and spades
+ * respectively.
+ */
 class Card {
   private:
     int value;
 
   public:
-    Card();
-    int getRank(Card c){};
-    int getCard();
+    Card(int thisValue){
+      value = thisValue;
+    };
+    int getRank(Card c){
+      return c.value % 13;
+    };
+    int getSuit(Card c){
+      return (c.value % 52) / 13;
+    };
+    void showCard(Card c){
+      switch (getRank(c)){
+        case 0:
+          cout << "Deuce of ";
+          break;
+        case 1:
+          cout << "Three of ";
+          break;
+        case 2:
+          cout << "Four of ";
+          break;
+        case 3:
+          cout << "Five of ";
+          break;
+        case 4:
+          cout << "Six of ";
+          break;
+        case 5:
+          cout << "Seven of ";
+          break;
+        case 6:
+          cout << "Eight of ";
+          break;
+        case 7:
+          cout << "Nine of ";
+          break;
+        case 8:
+          cout << "Ten of ";
+          break;
+        case 9:
+          cout << "Jack of ";
+          break;
+        case 10:
+          cout << "Queen of ";
+          break;
+        case 11:
+          cout << "King of ";
+          break;
+        case 12:
+          cout << "Ace of ";
+          break;
+      }
+
+      switch (getSuit(c)){
+        case 0:
+          cout << "Clubs\n";
+          break;
+        case 1:
+          cout << "Diamonds\n";
+          break;
+        case 2:
+          cout << "Hearts\n";
+          break;
+        case 3:
+          cout << "Spades\n";
+          break;
+      }
+    };
 };
 
 class Deck {
   public:
-    Deck();
+    Deck(){
+      queue<int> A, B;
+    };
     void shuffle();
-    Card dealCard();
+    void dealCard(queue<int> Deck){
+    };
     void playCard();
 };
 
